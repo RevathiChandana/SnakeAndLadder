5c048f25bee298f27c3790884927d82fd1008f4e
Revathi Chandana <chandanarevathi30@gmail.com>
2020-07-12 12:32:54 +0530

    [REVATHI]Add : the player checks for no play, ladder or snake

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -2,4 +2,20 @@
 echo "==========Snake and Ladder=========="
 startPosition=0;
 endPosition=100;
-echo "$((1+$(($RANDOM%6))))"
+pointOfGame=$((1+$(($RANDOM%6))))
+function snakeAndLadder(){
+	case $(($RANDOM%3)) in
+		0) echo "No Play"
+			echo "player position	-	$startPosition";;
+		1) echo "player get ladder";
+			startPosition=$(($startPosition+$pointOfGame));
+			echo "player position	-	$startPosition";;
+		2)	echo "player get snake";
+			if [[ $startPOsition -ge $pointOfGame ]]
+			then
+				startPosition=$(($startPosition-$pointOfGame));
+			fi
+			echo "player position	-	$startPosition"
+	esac
+}
+snakeAndLadder
