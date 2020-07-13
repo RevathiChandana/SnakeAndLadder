2f4cd627440d220e2c97819efbfc105de7a92643
Revathi Chandana <chandanarevathi30@gmail.com>
2020-07-13 19:11:22 +0530

    [REVATHI]refactor: updated constant variables

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -1,21 +1,21 @@
 #! /bin/bash
 echo "==========Snake and Ladder=========="
-startPosition=0;
-endPosition=100;
+START_POSITION=0;
+END_POSITION=100;
 pointOfGame=$((1+$(($RANDOM%6))))
 function snakeAndLadder(){
 	case $(($RANDOM%3)) in
 		0) echo "No Play"
-			echo "player position	-	$startPosition";;
+			echo "player position	-	$START_POSITION";;
 		1) echo "player get ladder";
-			startPosition=$(($startPosition+$pointOfGame));
-			echo "player position	-	$startPosition";;
+			START_POSITION=$(($START_POSITION+$pointOfGame));
+			echo "player position	-	$START_POSITION";;
 		2)	echo "player get snake";
-			if [[ $startPOsition -ge $pointOfGame ]]
+			if [[ $START_POSITION -ge $pointOfGame ]]
 			then
-				startPosition=$(($startPosition-$pointOfGame));
+				START_POSITION=$(($START_POSITION-$pointOfGame));
 			fi
-			echo "player position	-	$startPosition"
+			echo "player position	-	$START_POSITION"
 	esac
 }
 snakeAndLadder
