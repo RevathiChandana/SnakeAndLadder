972b7d7611e1a2b2c6b9a17261d7e23a2d12252f
Revathi Chandana <chandanarevathi30@gmail.com>
2020-07-13 19:19:15 +0530

    [REVATHI]refactor: changes done on constant variables

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -1,28 +1,28 @@
 #! /bin/bash
 echo "==========Snake and Ladder=========="
-startPosition=0;
-endPosition=100;
+START_POSITION=0;
+END_POSITION=100;
 function snakeAndLadder(){
-	while [ $startPosition -ne $endPosition ]
+	while [ $START_POSITION -ne $END_POSITION ]
 	do
 	dieNumber=$((1+$(($RANDOM%6))))
 	case $(($RANDOM%3)) in
-		0) echo "No play($dieNumber)  	-	player position	-	$startPosition";;
-		1) pointOfGame=$(($startPosition+$dieNumber));
-			if [[	$pointOfGame -eq $endPosition ]]
+		0) echo "No play($dieNumber)  	-	player position	-	$START_POSITION";;
+		1) pointOfGame=$(($START_POSITION+$dieNumber));
+			if [[	$pointOfGame -eq $END_POSITION ]]
 			then
 				echo "getLadder($dieNumber)   -  player position   -  $pointOfGame";
 				echo "the player wins";exit;
-			elif [[ $pointOfGame -lt $endPosition ]]
+			elif [[ $pointOfGame -lt $END_POSITION ]]
 			then
-				startPosition=$pointOfGame;
+				START_POSITION=$pointOfGame;
 			fi
-			echo "getLadder($dieNumber)	-	player position	-	$startPosition";;
-		2)	if [[ $startPosition -ge $dieNumber ]]
+			echo "getLadder($dieNumber)	-	player position	-	$START_POSITION";;
+		2)	if [[ $START_POSITION -ge $dieNumber ]]
 			then
-				startPosition=$(($startPosition-$dieNumber));
+				START_POSITION=$(($START_POSITION-$dieNumber));
 			fi
-			echo "getSanke($dieNumber)	-	player position	-	$startPosition"
+			echo "getSanke($dieNumber)	-	player position	-	$START_POSITION"
 	esac
 	done
 }
